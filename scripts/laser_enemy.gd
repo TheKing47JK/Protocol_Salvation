abb25d709222d360effa1374a59c30588971f594
Ong Qi Cheng <ongqicheng@gmail.com>
2025-07-28 22:09:33 +0800

    play will take damage (max 4) to enemy laser

--- scripts/laser_enemy.gd
+++ scripts/laser_enemy.gd
@@ -12,5 +12,5 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is ShipPlayer:
-		body.die()
+		body.take_damage(1)
 		queue_free()
--- scripts/ship.gd
+++ scripts/ship.gd
@@ -2,36 +2,43 @@ class_name ShipPlayer
 extends CharacterBody2D
 
 signal laser_shoot(laser_scene, location)
+signal life_changed
+
+@export var armor: int = 4
 @export var speed = 500
 @export var rateOfFire = 0.25
+
 @onready var muzzle: Marker2D = $Muzzle
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 var shoot_cd := false
-
 var laser_scene = preload("res://scenes/laser.tscn")
 
+
 func _process(_delta: float) -> void:
-	if Input.is_action_pressed("shoot"):
-		if !shoot_cd:
-			shoot_cd = true
-			shoot()
-			await get_tree().create_timer(rateOfFire).timeout
-			shoot_cd = false
+	if Input.is_action_pressed("shoot") and not shoot_cd:
+		shoot_cd = true
+		shoot()
+		await get_tree().create_timer(rateOfFire).timeout
+		shoot_cd = false
 	animate_the_ship()
-	
+
+
 func _physics_process(_delta: float) -> void:
-	var direction = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
+	var direction = Vector2(
+		Input.get_axis("move_left", "move_right"),
+		Input.get_axis("move_up", "move_down")
+	)
 	velocity = direction * speed
-	
-	#make sure the player cannot go out of screen
-	var viewRect :=get_viewport_rect()
-	position.x = clamp(position.x,0,viewRect.size.x)
-	position.y = clamp(position.y,0,viewRect.size.y)
+	var viewRect := get_viewport_rect()
+	position.x = clamp(position.x, 0, viewRect.size.x)
+	position.y = clamp(position.y, 0, viewRect.size.y)
 	move_and_slide()
-	
+
+
 func shoot():
 	laser_shoot.emit(laser_scene, muzzle.global_position)
+
 
 func animate_the_ship() -> void:
 	if Input.is_action_pressed("move_right"):
@@ -40,7 +47,14 @@ func animate_the_ship() -> void:
 		animated_sprite_2d.play("move_left")
 	else:
 		animated_sprite_2d.play("center")
-	pass
+
+
+func take_damage(amount: int) -> void:
+	armor -= amount
+	life_changed.emit(armor)
+	if armor <= 0:
+		die()
+
 
 func die():
 	queue_free()
