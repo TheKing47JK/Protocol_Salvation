bf3a9ac67b30518862070849165741e07cf9257b
Yilongma <yl1997@1utar.my>
2025-08-23 04:25:09 +0800

    Update rowspawner.gd

--- scripts/rowspawner.gd
+++ scripts/rowspawner.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-#The scene
+#Import enemy scene
 @export var enemy_scene: PackedScene
 @export var count: int = 5
 @export var start_x: float = 100
@@ -9,7 +9,7 @@ extends Node2D
 @onready var row_spawn_timer: Timer = $rowtimer
 
 func _ready():
-	# Setup timer
+	# Setup timer for 3s
 	row_spawn_timer.wait_time = 3.0
 	row_spawn_timer.one_shot = true
 	add_child(row_spawn_timer)
@@ -18,14 +18,17 @@ func _ready():
 	print("RowSpawner: Timer started, will spawn after 3s")
 
 func _on_timer_timeout():
+	#Time up and spawn the enemy
 	print("RowSpawner: Timer finished at ", Time.get_ticks_msec() / 1000.0, "s")
 	spawn_row()
 
 func spawn_row():
+	#Error checking
 	if enemy_scene == null:
 		push_warning("RowSpawner: No enemy_scene assigned!")
 		return
-
+	
+	#For loop to spawn 5 enemy
 	for i in range(count):
 		var enemy: Node2D = enemy_scene.instantiate()
 		enemy.position = Vector2(start_x + i * gap, y_pos)
