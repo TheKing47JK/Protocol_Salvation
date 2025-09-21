90de47dd4c2d73a837c27d4b6bd7a76900146a82
Ong Qi Cheng <ongqicheng@gmail.com>
2025-09-21 14:21:46 +0800

    setting fix for volume

--- HUD/settings.gd
+++ HUD/settings.gd
@@ -3,26 +3,32 @@ extends Control
 var master_bus_id: int
 var music_bus_id: int
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	master_bus_id = AudioServer.get_bus_index("Master")
 	music_bus_id = AudioServer.get_bus_index("Music")
-	pass # Replace with function body.
+
+	# Default both sliders to 50%
+	$setting_panel/control/Master.value = 0.5
+	$setting_panel/control/Music.value = 0.5
+
+	# Apply the default volumes to the buses
+	AudioServer.set_bus_volume_db(master_bus_id, linear_to_db($setting_panel/control/Master.value / 1.0))
+	AudioServer.set_bus_volume_db(music_bus_id, linear_to_db($setting_panel/control/Music.value / 1.0))
+
+
 
 func _on_mute_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(0,toggled_on)
-	pass # Replace with function body.
+	AudioServer.set_bus_mute(master_bus_id, toggled_on)
+
 
 func _on_master_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
 	AudioServer.set_bus_volume_db(master_bus_id, db)
-	pass # Replace with function body.
 
 
 func _on_music_value_changed(value: float) -> void:
 	var db = linear_to_db(value)
 	AudioServer.set_bus_volume_db(music_bus_id, db)
-	pass # Replace with function body.
 
 
 func _on_btn_confirm_pressed() -> void:
@@ -33,5 +39,3 @@ func _on_btn_confirm_pressed() -> void:
 	AudioServer.set_bus_volume_db(music_bus_id, linear_to_db(music_value))
 	
 	TransitionManager.transition_to("res://stage/stage_main.tscn")
-
-	pass # Replace with function body.
